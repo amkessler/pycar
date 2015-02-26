8c6ec8081cb3ece54a125bc80eb77ddaf2abf036
Ryan Nagle <rmnagle@gmail.com>
2015-02-26 11:25:57 -0600

    update the sample data, update baseball_complete.py making sure everything works as expected, make baseball.py a kind of fill in the blanks puzzle >_<

--- project2/baseball.py
+++ project2/baseball.py
@@ -1,93 +1,112 @@
-#imports
+# Imports
 
-#First, let's see what kind of data we have to work with
+# First, let's get the ID of each player in the top 10-percentile
+def calculate_top10(filename):
+    # Open the salary csv
 
-    #Open the salary csv
-   
-    #Make the file object usable
-   
-    #Create your header row and look at what is in here
-   
-    #Find the index of the year and player id
+    # Read the opened csv file with csv.reader
 
-    #Check the type of the year column to see if it is a string or int
+    # Arrange in descending order of salary
+    # Remember that lists are ordered!
 
-    #Because we're on the first row of data, we need to 
-    #return to the top before we do anything with this.
-    #We do this by resetting the pointer in the original file.
+    # Create a list of the top 10%
 
-    #Arrange in descending order of salary
-    #Remember that lists always keep their order!
+    # Round it!
 
-    #Create a list of the top 10%
+    # We don't want decimal points (you can't have part of a player) so cast to an int
 
-    #Round it!
+    # Now let's create our final list, of just the highest-paid players
+    top_10_percentile_ids = []
 
-    #We don't want decimal points (you can't have part of a player)
-    #so cast to an int
+    # We only need the player IDs right now.
+    # Loop over the sorted salaries and extract the player ID, adding it to the
+    # top_10_percentile_ids list.
 
-    #You could do the above steps in one line like this:
-    #int(math.floor(len(sorted_salaries * .10)))
-
-    #Now let's create our final list, of just the highest-paid players
-
-    #We only need the player IDs right now.
+    # Return the top_10_percentile_ids list
+    return top_10_percentile_ids
 
 
-#We are going to be working with dictionaries to make things easier
+# We are going to be working with dictionaries to make things easier
+def create_salary_dict(filename, top_10_percentile_ids):
+    # Open the csv
+    salaries_object = open(filename, 'rb')
 
-    #Open the csv
+    # This time, let's use DictReader, which maps the header row's values to each item in each row.
+    player_dict = csv.DictReader(salaries_object)
 
-    #This time, let's use DictReader,
-    #which maps the header row's values to each item in each row
+    # Create new list of only 2013 information
+    # NOTE: You can't start a variable with a number, so 2013_salaries won't work
+    salaries_2013 = {}
 
-    #Create new list of only 2013 information
-    #NOTE: You can't start a variable with a number, so 2013_salaries won't work
+    for row in player_dict:
+        # Using DictReader allows us to access rows by their column name!
+       year = row["yearID"]
+       if year == '2013':
+           # Create a record for each player's ID and assign it the salary
+           salaries_2013[row["playerID"]] = row["salary"]
 
-        #Using DictReader allows us to access rows by their column name!
+    # Now we can reference the salary of any player whose ID we know.
+    # But we only want those who were in the top 10% of all time.
+    # Create a new dict to hold just the top players from 2013
+    top_salaries_2013 = {}
 
-           #Create a record for each player's ID and assign it the salary
+    # Let's compare our player dict with the list of all-time
+    # high salaries we made in the first function.
+    # (You could combine this step with the DictReader step above.)
+    for player in top_10_percentile_ids:
+        # Check for the presence of a key that matches the playerID in salaries_2013
+        if player in salaries_2013:
+            top_salaries_2013[player] = { "salary": salaries_2013[player] }
 
-    #Now we can reference the salary of any player whose ID we know.
-    #But we only want those who were in the top 10% of all time.
-    #Create a new dict to hold just the top players from 2013
-
-
-    #Let's compare our player dict with the list of all-time 
-    #high salaries we made in the first function.
-    #(You could combine this step with the DictReader step above.)
-
-        #Check for the presence of a key that matches the playerID in salaries_2013
-
-
-#new function
-
-    #Open the master csv
-
-    #Read the file
-
-    #Let's look at one record of the master data to get the headers
-
-    #That's a little hard to read, isn't it? Try prettyprint instead.
-
-    #Reset the generator and skip the header row
-
-    #Create a dict of the master file with DictReader
-
-    #Assemble the troops
-
-    #Loop over the top salaries dict to find the player IDs in the master dict
-    #We could also loop over the master dict to find which of those exist
-    #in the top salaries dict, but that would be less efficient.
-    #When you loop over a dict, you only have access to the keys.
-    #To access the values, we need .iteritems()
-    #Remember the key is the player ID and the value is the salary.
-    #Typically, when iterating over a dict, the syntax is:
-    #for key, value in my_dict.iteritems():
-    #For clarity, we will use the header row values instead.
+    return top_salaries_2013
 
 
-    #Add names, birth state and birth country to the dict
+def add_player_stats(master_file, top_salaries_dict):
+    # Open the master csv
 
+    # Read the file
 
-#write to a file
+    # Skip the header row
+
+    # Create a master data dictionary
+
+    # Add a key to master_dict for each row in the master data csv file.
+    # The key will be the player ID. The value will be a dictionary with
+    # keys and values for each of the headers in the master file.
+
+    # Next we'll loop over top_salaries_dict to find the player IDs in master_dict.
+    #
+    # Remember, in top_salaries_dict, the key is the player ID and the value is the salary.
+    #
+    # Note: when you loop over a dict, you only have access to the keys.
+    #
+    # To access the values, we need .iteritems()
+    #
+    # Typically, when iterating over a dict, the syntax is:
+    #
+    #   for key, value in my_dict.iteritems():
+    #
+    # Add names, birth state and birth country to the top_salaries_dict. This is effectively a join
+    # between some of the data in the master file and some of the data in the salaries file.
+
+    # Return modified top_salaries_dict
+    return top_salaries_dict
+
+# The location of the data files
+salary_file = 'data/2014/Salaries.csv'
+master_file = 'data/2014/Master.csv'
+
+# Call calculate_top10, passing salary_file. Store the return value in a variable
+top10 = calculate_top10(salary_file)
+# Pretty-print the return value
+pprint(top10)
+
+# Call create_salary_dict, passing salary_file and the return value of our call to calculate_top10.
+top_salaries_dict = create_salary_dict(salary_file, top10)
+# Pretty-print the return value
+pprint(top_salaries_dict)
+
+# Call add_player_stats, passing master_file and the return value of our call to create_salary_dict.
+salaries_plus_player_stats = add_player_stats(master_file, top_salaries_dict)
+# Pretty-print the result
+pprint(salaries_plus_player_stats)
--- project2/baseball_complete.py
+++ project2/baseball_complete.py
@@ -1,117 +1,99 @@
+# Imports
 import csv
 import operator
 import math
 from pprint import pprint
 
 
-#First, let's see what kind of data we have to work with
-def calculate_top10 (filename):
-    #Open the salary csv
+# First, let's get the ID of each player in the top 10-percentile
+def calculate_top10(filename):
+    # Open the salary csv
     salaries_object = open(filename, 'rb')
 
-    #Make the file object usable
+    # Read the opened csv file with csv.reader
     salary_data = csv.reader(salaries_object)
 
-    #Create your header row and look at what is in here
-    header_row = salary_data.next()
-
-    #Find the index of the year and player id
-    print header_row
-
-    #Check the type of the year column to see if it is a string or int
-    sample_data = salary_data.next()
-    print '%s is %s' % (sample_data[0], type(sample_data[0]))
-
-    #Because we're on the first row of data, we need to 
-    #return to the top before we do anything with this.
-    #We do this by resetting the pointer in the original file.
-    salaries_object.seek(0)
-
-    #Arrange in descending order of salary
-    #Remember that lists always keep their order!
+    # Arrange in descending order of salary
+    # Remember that lists are ordered!
     sorted_salaries = sorted(salary_data, key=operator.itemgetter(4), reverse=True)
 
-    #Create a list of the top 10%
+    # Create a list of the top 10%
     top_percentile = len(sorted_salaries) * .10
 
-    #Round it!
+    # Round it!
     rounded_salaries = math.floor(top_percentile)
 
-    #We don't want decimal points (you can't have part of a player)
-    #so cast to an int
+    # We don't want decimal points (you can't have part of a player) so cast to an int
     int_salaries = int(rounded_salaries)
 
-    #You could do the above steps in one line like this:
-    #int(math.floor(len(sorted_salaries * .10)))
+    # You could do the above steps in one line like this:
+    # int(math.floor(len(sorted_salaries * .10)))
 
-    #Now let's create our final list, of just the highest-paid players
-    cream_of_the_crop = []
+    # Now let's create our final list, of just the highest-paid players
+    top_10_percentile_ids = []
 
-    #We only need the player IDs right now.
+    # We only need the player IDs right now.
+    # Loop over the sorted salaries and extract the player ID, adding it to the
+    # top_10_percentile_ids list.
     for index, row in enumerate(sorted_salaries):
         if index > 0 and index <= int_salaries:
-            cream_of_the_crop.append(row[3])
+            top_10_percentile_ids.append(row[3])
 
-    return cream_of_the_crop
+    # Return the top_10_percentile_ids list
+    return top_10_percentile_ids
 
 
-#We are going to be working with dictionaries to make things easier
-def create_salary_dict(filename, cream_of_the_crop):
-    #Open the csv
+# We are going to be working with dictionaries to make things easier
+def create_salary_dict(filename, top_10_percentile_ids):
+    # Open the csv
     salaries_object = open(filename, 'rb')
 
-    #This time, let's use DictReader,
-    #which maps the header row's values to each item in each row
+    # This time, let's use DictReader, which maps the header row's values to each item in each row.
     player_dict = csv.DictReader(salaries_object)
 
-    #Create new list of only 2013 information
-    #NOTE: You can't start a variable with a number, so 2013_salaries won't work
+    # Create new list of only 2013 information
+    # NOTE: You can't start a variable with a number, so 2013_salaries won't work
     salaries_2013 = {}
 
     for row in player_dict:
-        #Using DictReader allows us to access rows by their column name!
+        # Using DictReader allows us to access rows by their column name!
        year = row["yearID"]
        if year == '2013':
-           #Create a record for each player's ID and assign it the salary
+           # Create a record for each player's ID and assign it the salary
            salaries_2013[row["playerID"]] = row["salary"]
 
-    #Now we can reference the salary of any player whose ID we know.
-    #But we only want those who were in the top 10% of all time.
-    #Create a new dict to hold just the top players from 2013
+    # Now we can reference the salary of any player whose ID we know.
+    # But we only want those who were in the top 10% of all time.
+    # Create a new dict to hold just the top players from 2013
     top_salaries_2013 = {}
 
-    #Let's compare our player dict with the list of all-time 
-    #high salaries we made in the first function.
-    #(You could combine this step with the DictReader step above.)
-    for player in cream_of_the_crop:
-        #Check for the presence of a key that matches the playerID in salaries_2013
+    # Let's compare our player dict with the list of all-time
+    # high salaries we made in the first function.
+    # (You could combine this step with the DictReader step above.)
+    for player in top_10_percentile_ids:
+        # Check for the presence of a key that matches the playerID in salaries_2013
         if player in salaries_2013:
             top_salaries_2013[player] = { "salary": salaries_2013[player] }
 
     return top_salaries_2013
 
 
-def add_player_stats(top_salaries_dict, master_file):
-    #Open the master csv
+def add_player_stats(master_file, top_salaries_dict):
+    # Open the master csv
     master_object = open(master_file, 'rb')
 
-    #Read the file
+    # Read the file
     master_data = csv.DictReader(master_object)
 
-    #Let's look at one record of the master data to get the headers
-    print master_data.next()
-
-    #That's a little hard to read, isn't it? Try prettyprint instead.
-    pprint(master_data.next())
-
-    #Reset the generator and skip the header row
-    master_object.seek(0)
+    # Skip the header row
     master_data.next()
 
-    #Create a dict of the master file with DictReader
+    # Create a master data dictionary
     master_dict = {}
 
-    #Assemble the troops
+    # Add a key to master_dict for each row in the master_data csv file.
+    # The key will be the player ID. The value will be a dictionary with
+    # keys and values for each of the headers in the master_file.
     for row in master_data:
         master_dict[row["playerID"]] = {
             "first_name": row["nameFirst"],
@@ -133,35 +115,45 @@ def add_player_stats(top_salaries_dict, master_file):
             "final_game": row["finalGame"]
         }
 
-    #Loop over the top salaries dict to find the player IDs in the master dict
-    #We could also loop over the master dict to find which of those exist
-    #in the top salaries dict, but that would be less efficient.
-    #When you loop over a dict, you only have access to the keys.
-    #To access the values, we need .iteritems()
-    #Remember the key is the player ID and the value is the salary.
-    #Typically, when iterating over a dict, the syntax is:
-    #for key, value in my_dict.iteritems():
-    #For clarity, we will use the header row values instead.
-
-
-    #Add names, birth state and birth country to the dict
+    # Next we'll loop over top_salaries_dict to find the player IDs in master_dict.
+    #
+    # Remember, in top_salaries_dict, the key is the player ID and the value is the salary.
+    #
+    # Note: when you loop over a dict, you only have access to the keys.
+    #
+    # To access the values, we need .iteritems()
+    #
+    # Typically, when iterating over a dict, the syntax is:
+    #
+    #   for key, value in my_dict.iteritems():
+    #
+    # Add names, birth state and birth country to the top_salaries_dict. This is effectively a join
+    # between some of the data in the master file and some of the data in the salaries file.
     for playerID, salary in top_salaries_dict.iteritems():
-        top_salaries_dict.update({ playerID: {
+        top_salaries_dict[playerID].update({
             'first_name':  master_dict[playerID]["first_name"],
             'last_name': master_dict[playerID]["last_name"],
             'birth_state': master_dict[playerID]["birth_state"],
-            'birth_country': master_dict[playerID]["birth_country"]}
+            'birth_country': master_dict[playerID]["birth_country"]
         })
 
     return top_salaries_dict
 
-salary_file = 'data/2013/Salaries.csv'
+# The location of the data files
+salary_file = 'data/2014/Salaries.csv'
+master_file = 'data/2014/Master.csv'
 
-#TODO: Make this so we can walk a folder structure
-master_file = 'data/2013/Master.csv'
-
+# Call calculate_top10, passing salary_file. Store the return value in a variable
 top10 = calculate_top10(salary_file)
-top_salaries_dict = create_salary_dict(top10)
-final_file = add_player_stats(top_salaries_dict, master_file)
+# Pretty-print the return value
+pprint(top10)
 
-#write to a file
+# Call create_salary_dict, passing salary_file and the return value of our call to calculate_top10.
+top_salaries_dict = create_salary_dict(salary_file, top10)
+# Pretty-print the return value
+pprint(top_salaries_dict)
+
+# Call add_player_stats, passing master_file and the return value of our call to create_salary_dict.
+salaries_plus_player_stats = add_player_stats(master_file, top_salaries_dict)
+# Pretty-print the result
+pprint(salaries_plus_player_stats)
