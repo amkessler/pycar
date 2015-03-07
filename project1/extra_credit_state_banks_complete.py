cb49186d91f0b98690b525986f34097696ae1c8d
Aaron Williams <aaron.colby.williams@gmail.com>
2015-03-07 14:51:00 -0800

    functions!

--- project1/extra_credit_state_banks_complete.py
+++ project1/extra_credit_state_banks_complete.py
@@ -4,36 +4,47 @@
 import csv
 import us
 
-# We're going to download a csv file...
-# What should we name it?
-file_name = "banklist.csv"
+def break_apart_states_into_files():
+    # We're going to download a csv file...
+    # What should we name it?
+    file_name = "banklist.csv"
 
-# loop over all US states
-for state in us.states.STATES:
-    # Open the csv file
-    with open(file_name, "rb") as file:
+    # loop over all US states
+    for state in us.states.STATES:
+        # Open the csv file
+        with open(file_name, "rb") as file:
 
-        # Use python's csv reader to access the contents
-        # and create an object that represents the data
-        csv_data = csv.reader(file)
-        headers = csv_data.next()
-        # create csv based on state name
-        filename = state.name + '_banks.csv'
-        # create the file
-        output_file = open(filename, 'wb')
-        # create the writer with commas as delimiter
-        writer = csv.writer(output_file, delimiter=',')
-        # write the headers
-        writer.writerow(headers)
+            # Use python's csv reader to access the contents
+            # and create an object that represents the data
+            csv_data = csv.reader(file)
+            headers = csv_data.next()
+            # create csv based on state name
+            filename = state.name + '_banks.csv'
+            # create the file
+            output_file = open(filename, 'wb')
+            # create the writer with commas as delimiter
+            writer = csv.writer(output_file, delimiter=',')
+            # write the headers
+            writer.writerow(headers)
 
-        # Loop through each row of the csv...
-        for row in csv_data:
-            # assign state abbr
-            this_state = row[2]
-            # check if states match
-            if this_state == state.abbr:
-                # write the row
-                writer.writerow(row)
+            # Loop through each row of the csv...
+            for row in csv_data:
+                # assign state abbr
+                this_state = row[2]
+                # check if states match
+                if this_state == state.abbr:
+                    # write the row
+                    writer.writerow(row)
 
-        # close the file
-        output_file.close()
+            # close the file
+            output_file.close()
+
+def compare_states_to_nation(state):
+    for state in us.states.STATES:
+        pass
+
+def blastoff():
+    break_apart_states_into_files()
+    compare_states_to_nation()
+
+blastoff()
